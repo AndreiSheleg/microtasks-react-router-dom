c9e2ac544d3baddd52ed63b81a38cc79605f6cd4
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-08-27 18:28:13 +0300

    refactor name of Components, import images

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
-import {PageOne} from "./components/pages/PageOne";
-import {PageTwo} from "./components/pages/PageTwo";
-import {PageThree} from "./components/pages/PageThree";
+import {Adidas} from "./components/pages/Adidas";
+import {Puma} from "./components/pages/Puma";
+import {Abibas} from "./components/pages/Abibas";
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
+import styled from "styled-components";
+import {S} from './components/pages/_styles'
 
-
+const PATH = {
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+    ERROR404: '/error404',
+} as const
 function App() {
     return (
         <div>
@@ -14,20 +21,20 @@ function App() {
             <div className={styles.body}>
                 <div className={styles.nav}>
                     Здесь будет навигация:
-                    <div><NavLink to={'/page-1'} className={({isActive}) => isActive ? styles.active : styles.navLink } >-Page-1</NavLink></div>
-                    <div><NavLink to={'/page-2'} className={({isActive}) => isActive ? styles.active : styles.navLink }>-Page-2</NavLink></div>
-                    <div><NavLink to={'/page-3'} className={({isActive}) => isActive ? styles.active : styles.navLink }>-Page-3</NavLink></div>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>-Adidas-</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>-Puma-</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>-Abibas-</NavLink></S.NavWrapper>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route path="/" element={<Navigate to={'/page-1'}/>}/>
+                        <Route path="/" element={<Navigate to={'/adidas'}/>}/>
 
-                        <Route path="/page-1" element={<PageOne/>}/>
-                        <Route path="/page-2" element={<PageTwo/>}/>
-                        <Route path="/page-3" element={<PageThree/>}/>
-                        <Route path="/error404" element={<Error404/>}/>
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path={PATH.ERROR404} element={<Error404/>}/>
 
-                        <Route path="/*" element={<Navigate to={'/error404'} />}/>
+                        <Route path="/*" element={<Navigate to={PATH.ERROR404} />}/>
 
                     </Routes>
 
@@ -38,5 +45,5 @@ function App() {
     );
 }
 
-
 export default App;
+
